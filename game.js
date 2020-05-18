7f281736fb3ee3ac241efd457cdbbc306ddfada4
Bruno Belarmino <brunobelarm.nog@gmail.com>
2020-05-18 20:38:09 -0300

    mosquito random inserido
    Adc o elemento html posicionado de forma aleatoria.

--- game.js
+++ game.js
@@ -1,8 +1,28 @@
+//Gerar a dimensão da tela automaticamente, deixando o palco do jogo dinamico caso o usuario a redimensione:
 var altura, largura
 
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
     largura = window.innerWidth
-    console.log(altura, largura)
+    console.log(largura, altura)
 }
 ajustaTamanhoPalcoJogo()
+
+//posição randomica do mosquito:
+var posicaoX = Math.floor(Math.random() * largura) - 90 //largura
+var posicaoY = Math.floor(Math.random() * altura) - 90  //altura
+
+posicaoX = posicaoX < 0 ? 0 : posicaoX
+posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+console.log(posicaoX, posicaoY)
+
+//criar o elemento html da imagem do mosquito:
+var mosquito = document.createElement('img')
+mosquito.src = './img/mosca.png'
+mosquito.className = 'mosquito1'
+mosquito.style.left = posicaoX + 'px'
+mosquito.style.top = posicaoY + 'px'
+mosquito.style.position = 'absolute'
+
+document.body.appendChild(mosquito)
