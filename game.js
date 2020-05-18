8f8fc9faf3b58b04bbb247e565d13504006c80d2
Bruno Belarmino <brunobelarm.nog@gmail.com>
2020-05-18 20:02:22 -0300

    palco dinamico
    Adc function ajustaTamanhoPalcoJogo()

---
+++ game.js
@@ -0,0 +1,8 @@
+var altura, largura
+
+function ajustaTamanhoPalcoJogo() {
+    altura = window.innerHeight
+    largura = window.innerWidth
+    console.log(altura, largura)
+}
+ajustaTamanhoPalcoJogo()
